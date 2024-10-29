0a6c0ffbcc935ad598452ba2bbf02031b65262c9
nimmusiima <abangiraprecious@gmail.com>
2024-10-29 09:27:27 +0300

    constraints

--- constraints.sql
+++ constraints.sql
@@ -3,7 +3,56 @@ CREATE DATABASE School
 USE School;
 
 CREATE TABLE Student (
-    Regno VARCHAR(15)
-    AccessNo VARCHAR(10)
-    Name
-)
+    Regno VARCHAR(15),
+    AccessNo VARCHAR(10),
+    StudentName VARCHAR(25),
+    TelNo INT,
+    AmountPaid INT
+);
+
+DESCRIBE Student;
+
+ALTER TABLE Student
+ADD CONSTRAINT Con1
+PRIMARY KEY (Regno);
+
+ALTER TABLE Student
+ADD CONSTRAINT Con2
+CHECK (LENGTH(RegNo) > 9);
+
+#NOT NULL AND NULL constraint can only be modified inline
+ALTER TABLE Student
+MODIFY AccessNo VARCHAR(10) NOT NULL;
+
+ALTER TABLE Student
+MODIFY StudentName VARCHAR(25) NOT NULL;
+
+ALTER TABLE Student
+ADD CONSTRAINT Con3
+CHECK (LENGTH(TelNo) = 10);
+
+ALTER TABLE Student
+MODIFY AmountPaid INT NOT NULL;
+
+ALTER TABLE Student
+ADD CONSTRAINT Con4
+CHECK (LENGTH(AmountPaid) > 200000);
+
+ALTER TABLE Student
+DROP CONSTRAINT Con4;
+
+ALTER TABLE Student
+ADD CONSTRAINT Con4
+CHECK (AmountPaid > 200000);
+
+ALTER TABLE Student
+ADD COLUMN Course VARCHAR(10);
+
+ALTER TABLE Student
+ADD CONSTRAINT Con5
+CHECK (Course IN('BSCS', 'BSDS', 'BSIT', 'DIT'));
+
+ALTER TABLE Student
+ADD CONSTRAINT Con6
+UNIQUE (AccessNo);
+
